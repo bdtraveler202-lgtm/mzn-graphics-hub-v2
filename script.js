a4adff7a7ead0c1513807d170f4ea5afed80b80f
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-12 02:50:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// MZN Graphics Hub
 
+console.log("MZN Graphics Hub Loaded Successfully");
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if(target){
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
+// Header Shadow
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector("header");
+
+    if(window.scrollY > 50){
+        header.style.boxShadow = "0 5px 20px rgba(0,0,0,.4)";
+    }else{
+        header.style.boxShadow = "none";
+    }
+
+});
